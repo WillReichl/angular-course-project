9480f0545d1fe04afc3d169f72e03b64c8b77be0
Will Reichl <William.Reichl@dcsg.com>
2018-10-30 08:04:44 -0400

    S3L52 - Created Ingredient Model in new Shared directory

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,4 @@
+export class Ingredient {
+    // TypeScript shortcut for listing out public properties individually and assigning them in the construction using this.XYZ = XYZ
+    constructor (public name: string, public amount: number) { }
+}
