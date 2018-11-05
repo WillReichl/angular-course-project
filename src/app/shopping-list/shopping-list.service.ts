f57a59079ffa93bffe2bd8991c884b3cd7cb4e90
Will Reichl <William.Reichl@dcsg.com>
2018-11-05 15:53:56 -0500

    S10L110 - Add Shopping List Service - My Solution

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ShoppingListService {
+export class ShoppingListService implements OnInit {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
 
   constructor() { }
+
+  ngOnInit() {
+    this.ingredientsChanged.emit(this.getIngredients());
+  }
+
+  getIngredients() {
+    return this.ingredients.slice(); // return copy
+  }
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.getIngredients());
+    console.log(this.ingredients);
+  }
 }
