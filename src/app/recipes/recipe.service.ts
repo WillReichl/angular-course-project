9b83916561058f21ced908ab06524cfbb1c17d60
Will Reichl <William.Reichl@dcsg.com>
2018-11-05 14:48:06 -0500

    S10L109 - Using Service for Cross-Component Communication, i/o property/event chain

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipes.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test.', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
     new Recipe('Test Recipe 2', 'I only like to eat candy.', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
