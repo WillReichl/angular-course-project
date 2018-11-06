2854153e2a05cd417b7391d11ee52e191efd821f
Will Reichl <William.Reichl@dcsg.com>
2018-11-05 20:21:43 -0500

    S10L112 - Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipes.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,25 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test.', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
-    new Recipe('Test Recipe 2', 'I only like to eat candy.', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      // tslint:disable-next-line:max-line-length
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Oscar_schnitzel_at_Grilli_Toro.jpg/1024px-Oscar_schnitzel_at_Grilli_Toro.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+      ),
+    new Recipe(
+      'Big Fat Burger',
+      'What else do you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/8/88/Windows_7_WHOPPER.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Ground Beef Patty', 2),
+        new Ingredient('Cheese', 2)
+      ])
   ];
 
   constructor() { }
