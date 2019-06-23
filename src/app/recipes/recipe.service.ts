47f924183f006dbb0b1ba26a8b948ec25275c740
Will Reichl <willreichl@gmail.com>
2019-06-23 09:52:50 -0400

    S12: Configuring Route Parameters - Get Recipe Detail Id from Route

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -15,11 +15,8 @@ export class RecipeService {
       'A super-tasty Schnitzel - just awesome!',
       // tslint:disable-next-line:max-line-length
       'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Oscar_schnitzel_at_Grilli_Toro.jpg/1024px-Oscar_schnitzel_at_Grilli_Toro.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]
-      ),
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+    ),
     new Recipe(
       'Big Fat Burger',
       'What else do you need to say?',
@@ -28,13 +25,18 @@ export class RecipeService {
         new Ingredient('Buns', 2),
         new Ingredient('Ground Beef Patty', 2),
         new Ingredient('Cheese', 2)
-      ])
+      ]
+    )
   ];
 
-  constructor(private shoppingListService: ShoppingListService) { }
+  constructor(private shoppingListService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice(); // return a *copy* of the array, cannot modify from outside
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
