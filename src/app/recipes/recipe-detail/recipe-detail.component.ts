d1ca823736a2df873edbc490cc3ea0ce4cbd2e4f
Will Reichl <willreichl@gmail.com>
2019-08-29 12:35:07 -0400

    S16: Redirect user after delete recipe

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -37,5 +37,6 @@ export class RecipeDetailComponent implements OnInit {
 
   public onDeleteRecipe() {
     this.recipeService.deleteRecipe(this.id);
+    this.router.navigate(['/recipes']);
   }
 }
