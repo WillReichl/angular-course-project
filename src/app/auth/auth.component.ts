f5a4c77aa6e0f310cfafd3e2adbc0432649f076f
Will Reichl <willreichl@gmail.com>
2019-11-09 21:24:44 -0500

    Added signup logic in auth component, along with loading spinner

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,19 +1,47 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService, AuthResponseData } from './auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
 export class AuthComponent {
+  constructor(private authService: AuthService) {}
+
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
 
   onSubmit(authForm: NgForm) {
-    console.log(authForm.value);
+    if (!authForm.valid) {
+      return;
+    }
+    if (this.isLoginMode) {
+      return;
+    } else {
+      const email = authForm.value.email;
+      const password = authForm.value.password;
+
+      this.isLoading = true;
+
+      this.authService.signUp(email, password).subscribe(
+        (authResponseData: AuthResponseData) => {
+          console.log(authResponseData);
+          this.isLoading = false;
+        },
+        error => {
+          console.log(error);
+          this.error = 'An error occurred!';
+          this.isLoading = false;
+        }
+      );
+    }
+
     authForm.reset();
   }
 }
