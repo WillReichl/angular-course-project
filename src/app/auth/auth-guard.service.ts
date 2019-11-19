9aa6651063daa7e07c53530ff1467367cf9cb068
Will Reichl <willreichl@gmail.com>
2019-11-18 21:54:24 -0500

    Add automatic redirect to auth route if not authenticated

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -2,25 +2,36 @@ import { Injectable } from '@angular/core';
 import {
   CanActivate,
   ActivatedRouteSnapshot,
-  RouterStateSnapshot
+  RouterStateSnapshot,
+  Router,
+  UrlTree
 } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): boolean | Observable<boolean> | Promise<boolean> {
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
     return this.authService.user.pipe(
+      take(1),
       map(user => {
-        return !!user; // user exists?
+        const isAuth = !!user; // user exists?
+        if (isAuth) {
+          return true;
+        }
+        return this.router.createUrlTree(['/auth']);
       })
     );
   }
