93183ab9f54ffec65df86ff12ea7f85141771f5b
Will Reichl <willreichl@gmail.com>
2019-11-25 08:11:37 -0500

    Creating a component programmatically (still not done, need entryComponent)

--- src/app/shared/placeholder/placeholder.directive.ts
+++ src/app/shared/placeholder/placeholder.directive.ts
@@ -4,5 +4,5 @@ import { Directive, ViewContainerRef } from '@angular/core';
   selector: '[appPlaceholder]'
 })
 export class PlaceholderDirective {
-  constructor(viewContainerRef: ViewContainerRef) {}
+  constructor(public viewContainerRef: ViewContainerRef) {}
 }
