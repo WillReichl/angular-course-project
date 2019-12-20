20b72017c80ad3db9974dcba1f977d7b0d272995
Will Reichl <William.Reichl@dcsg.com>
2019-12-20 11:31:55 -0500

    Setting up the NgRx Store
    * Added NgRx StoreModule to app module
    * Refactored imports in shopping list reducer and ran code formatter
    * Added action.payload so the actual payload will be added to the shopping list store

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,23 +1,21 @@
-import { Action } from '@ngrx/store';
-
 import { Ingredient } from '../../shared/ingredient.model';
-import { ADD_INGREDIENT } from './shopping-list.actions';
+import * as ShoppingListActions from './shopping-list.actions';
 
 const initialState = {
-  ingredients: [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
-  ]
+  ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)]
 };
 
 // if state is null, initialState is the default
-export function shoppingListReducer(state = initialState, action: Action) {
+export function shoppingListReducer(
+  state = initialState,
+  action: ShoppingListActions.AddIngredient
+) {
   switch (action.type) {
-    case ADD_INGREDIENT:
+    case ShoppingListActions.ADD_INGREDIENT:
       // return copy of old state + new data -- STATE SHOULD ALWAYS BE IMMUTABLE
       return {
         ...state,
-        ingredients: [...state.ingredients, action] // first step - we'll come back to this to get the actual ingredient to add
+        ingredients: [...state.ingredients, action.payload]
       };
   }
 }
