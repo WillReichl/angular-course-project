a8db4d67659f8b8aedf3637cc3bde3e479168865
Will Reichl <William.Reichl@dcsg.com>
2020-01-07 07:00:16 -0500

    Expanding the State
    Added new properties to the state managed by the shopping list reducer. Created interfaces to simplify references to shopping list's view of application state.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,6 +6,7 @@ import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 
 import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions';
+import * as fromShoppingList from '../shopping-list/store/shopping-list.reducer';
 
 @Injectable()
 export class RecipeService {
@@ -29,7 +30,7 @@ export class RecipeService {
   private recipes: Recipe[] = [];
 
   constructor(
-    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
+    private store: Store<fromShoppingList.AppState>
   ) {}
 
   getRecipes() {
