c25d263af90632022f7acecfe038df33ed7122f8
Will Reichl <willreichl@gmail.com>
2020-02-22 14:37:44 -0500

    Fix broken header logout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,6 +5,7 @@ import { Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
 
 import * as fromApp from '../store/app.reducer';
+import * as AuthActions from '../auth/store/auth.actions';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -49,6 +50,6 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   onLogout() {
-    this.authService.logout();
+    this.store.dispatch(new AuthActions.Logout());
   }
 }
